bdf66fc0793943582716252684cd5447aaf9c5a5
大城亮 <hiltoc.katsu@gmail.com>
2023-10-01 14:22:22 +0900

    ファイルサイズの表示

---
+++ 1prop-counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-file-input
+    show-size
+    counter
+    multiple
+    label="File input"
+  ></v-file-input>
+</template>
+<!--show-size:
+・ファイルを選択したときに、選択されたファイルのサイズを表示するオプションです。
+・このオプションが有効になっている場合、選択したファイルのサイズが表示されます。
+counter:
+・選択されたファイルの数をカウントして表示するオプションです。
+・たとえば、3つのファイルが選択された場合、"3 files" などのカウンターが表示されます。-->
